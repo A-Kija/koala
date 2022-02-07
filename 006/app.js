4aad0b6121b2549f7ecfd79d720c78e5f059e563
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-07 20:25:14 +0200

    1514

--- 006/app.js
+++ 006/app.js
@@ -39,3 +39,44 @@ for (let i = 1; i <= 50; i++) {
     }
 
 }
+
+console.log('------------------------------');
+
+let suma1 = 0; //Number
+let suma2 = ''; //String
+
+for (let i = 1; i < 16; i++) {
+    suma1 = suma1 + i;
+    suma2 = suma2 + i;
+}
+
+console.log(suma1, suma2);
+
+let stars = '';
+for (let i = 0; i < 40; i++) {
+    stars = stars + '*';
+}
+
+console.log(stars);
+
+
+
+console.log('------------------------------');
+
+
+let suma3 = ''; //String
+let suma4 = ''; //String
+
+for (let i = 1; i < 16; i++) {
+    suma3 = suma3 + i;
+    suma4 = i + suma4;
+}
+
+console.log(suma3, suma4);
+
+let suma5 = ''; //String
+for (let i = 15; i >= 1; i--) {
+    suma5 = suma5 + i;
+}
+
+console.log(suma5);
