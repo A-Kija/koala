99683dbee42de42699fd6630828e941816b7921a
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-08 19:17:34 +0200

    skaitiklis

--- Bond/app.js
+++ Bond/app.js
@@ -39,14 +39,18 @@ for (let i = 0; i < 5; i++) {
 console.log('-----------------');
 
 let suma2 = 0;
+let ratuSkaitiklis = 0;
 
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 17; i++) {
     let kauliukas = rand(1, 6);
     console.log(kauliukas);
     if (suma2 + kauliukas > 35) {
         break;
     }
     suma2 = suma2 + kauliukas;
+    ratuSkaitiklis++;
 }
 
 console.log('Suma: ', suma2);
+console.log('Ratu: ', ratuSkaitiklis);
+console.log('Baigta anksciau: ', 17 - ratuSkaitiklis);
