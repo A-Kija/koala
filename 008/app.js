d319135a618bd2c679a4379977965e0127485bed
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-09 18:51:10 +0200

    antanas switch

--- 008/app.js
+++ 008/app.js
@@ -33,3 +33,20 @@ switch (siuntinys) {
     default:
         console.log('galima dėti į XL');
 }
+
+let antanas = rand(1, 6);
+
+console.log('Antanas:', antanas);
+
+switch (antanas) {
+    case 1:
+    case 5:
+        console.log('TV');
+        break;
+    case 3:
+    case 4:
+        console.log('Skalbyklė');
+        break;
+    default:
+        console.log('Šaldyklė');
+}
