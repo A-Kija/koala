c2dd78ca8e2c25340d8cb85c6594b714dd7df516
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-10 18:41:14 +0200

    masyvas

---
+++ 009/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const namas = ['Petras', 'Birutė', 'Bebras'];
+
+console.log(typeof namas);
+
+console.log(namas[2]);
+console.log(namas[0]);
+
+namas[1] = 'Barsukas';
+
+namas[3] = 'Ūdra';
+
+namas[9] = 'Žirafa';
+
+namas[6] = true;
+
+namas[7] = [1, 2, 3];
+
+console.log(namas);
+
+for (let i = 0; i < namas.length; i++) {
+    console.log(namas[i]);
+}
