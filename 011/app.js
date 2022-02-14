cfc71ad384011f7c42d77242b7d8affaf4beb3e9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-14 19:05:22 +0200

    3 X 3

--- 011/app.js
+++ 011/app.js
@@ -18,3 +18,36 @@ for (let i = 0; i < BitGirls.length; i++) {
     console.log('-----------');
 
 }
+
+
+const bigHouse = [
+    ['Tomas', 'Sigute'],
+    ['Bebras', 'Bronius'],
+    ['Aiste', 'X'],
+];
+
+
+for (let i = 0; i < bigHouse.length; i++) {
+    let store = bigHouse[i];
+
+    for (let k = 0; k < store.length; k++) {
+        console.log(store[k]);
+    }
+}
+
+
+const _3X3 = [];
+
+
+for (let i = 0; i < 3; i++) {
+
+    const _3 = [];
+    for (let k = 0; k < 3; k++) {
+        _3.push('A');
+    }
+
+    _3X3.push(_3);
+
+}
+
+console.log(_3X3);
