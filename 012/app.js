dbf44ab752d979e0a740df6f3cbfed159adf56bc
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-15 21:16:22 +0200

    rand mas

--- 012/app.js
+++ 012/app.js
@@ -4,6 +4,12 @@ function pasisveikinimas(par2, par1) {
     console.log(par1, ', aš ', par2);
 }
 
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 console.log('Startas');
 
 let v = 'Jonas'
@@ -61,3 +67,14 @@ const calc = function(a, b, z) {
 
 console.log(calc(1, 2, '+'));
 console.log(calc(1, 2, '*'));
+
+
+const mas = function(c) {
+    const m = [];
+    for (let i = 0; i < c; i++) {
+        m.push(rand(0, 7))
+    }
+    return m;
+}
+
+console.log(mas(22));
