5569d2c77fde502abccd63c370807d004e687b47
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-15 19:50:54 +0200

    let function

--- 012/app.js
+++ 012/app.js
@@ -4,9 +4,6 @@ function pasisveikinimas(par2, par1) {
     console.log(par1, ', aš ', par2);
 }
 
-
-
-
 console.log('Startas');
 
 let v = 'Jonas'
@@ -19,10 +16,18 @@ pasisveikinimas('Murka', 'Ciao');
 console.log('---------------');
 
 
-
-
-
-
-
-
 console.log('Finišas');
+
+const dviejuSkaiciuSuma = function(a, b) {
+    let suma = a + b;
+    console.log(suma);
+}
+
+// const k = [dviejuSkaiciuSuma];
+
+// console.log(typeof dviejuSkaiciuSuma);
+
+dviejuSkaiciuSuma(5, 5);
+dviejuSkaiciuSuma(11, 12);
+
+// k[0](4, 6);
