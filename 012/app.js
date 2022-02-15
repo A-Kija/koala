b28a179e9f9a175da03ed94b56acf3dc65977ddd
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-15 19:18:32 +0200

    fun

---
+++ 012/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function pasisveikinimas(par2, par1) {
+    console.log(par1, ', aš ', par2);
+}
+
+
+
+
+console.log('Startas');
+
+let v = 'Jonas'
+
+pasisveikinimas(v, 'Hello');
+console.log('---------------');
+
+
+pasisveikinimas('Murka', 'Ciao');
+console.log('---------------');
+
+
+
+
+
+
+
+
+console.log('Finišas');
