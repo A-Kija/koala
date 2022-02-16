651c7e0c28e82cb511f76320921948dc247e0d72
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-16 11:30:46 +0200

    kodas

--- 01314/app.js
+++ 01314/app.js
@@ -16,3 +16,35 @@ const mas = c => {
 }
 
 console.log(mas(22));
+
+
+// const abcdz = () => 'abcdz' [rand(0, 4)];
+
+const abcdz = () => {
+    const word = 'abcdz';
+    const random = rand(0, 4);
+    return word[random];
+}
+
+const az = () => {
+    const word = 'qwertyuiopasdfghjklzxcvbnm';
+    const random = rand(0, word.length - 1);
+    return word[random];
+}
+
+console.log(abcdz());
+
+
+
+const line = l => {
+    let returnStr = '';
+    for (let i = 0; i < l; i++) {
+        returnStr = returnStr + az();
+    }
+    return returnStr;
+}
+
+
+
+
+console.log(line(4));
