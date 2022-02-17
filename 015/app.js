ba1dfd5eee23ecb857e6961b9662f1dae28fabb1
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-17 20:43:10 +0200

    cats

--- 015/app.js
+++ 015/app.js
@@ -6,63 +6,86 @@ function rand(min, max) {
 const bitGirls = ['Edita', 'Lina', 'Brigita', 'Deimantė', 'Justė'];
 const cats = ['Murka', 'Rainius', 'Meilutė', 'Bosas', 'Dičkis'];
 
-console.log('******************* 1 ******************');
-bitGirls.unshift('Nausėda');
-console.log(bitGirls);
+// console.log('******************* 1 ******************');
+// bitGirls.unshift('Nausėda');
+// console.log(bitGirls);
 
-console.log('******************* 2 ******************');
+// console.log('******************* 2 ******************');
 
-const bitCats = [];
+// const bitCats = [];
 
-cats.forEach(cat => {
-    const oneCat = [];
-    oneCat.push(cat);
-    oneCat.push(rand(0, 1) ? 'Storas' : 'Normalus');
-    bitCats.push(oneCat);
-});
-console.table(bitCats);
+// cats.forEach(cat => {
+//     const oneCat = [];
+//     oneCat.push(cat);
+//     oneCat.push(rand(0, 1) ? 'Storas' : 'Normalus');
+//     bitCats.push(oneCat);
+// });
+// console.table(bitCats);
 
-console.log('******************* 3 ******************');
+// console.log('******************* 3 ******************');
 
-let storas = 0;
-let normalus = 0;
+// let storas = 0;
+// let normalus = 0;
 
-bitCats.forEach(cat => {
-    if (cat[1] == 'Storas') {
-        storas++;
-    } else if (cat[1] == 'Normalus') {
-        normalus++;
-    }
-});
+// bitCats.forEach(cat => {
+//     if (cat[1] == 'Storas') {
+//         storas++;
+//     } else if (cat[1] == 'Normalus') {
+//         normalus++;
+//     }
+// });
 
-console.log('Stori:', storas, 'Normalūs:', normalus);
+// console.log('Stori:', storas, 'Normalūs:', normalus);
 
 
-console.log('******************* 4 ******************');
+// console.log('******************* 4 ******************');
 
-bitCats.sort((a, b) => {
-    if (a[0][1] > b[0][1]) {
-        return 1;
-    }
-    if (a[0][1] < b[0][1]) {
-        return -1;
-    }
-    return 0;
-});
+// bitCats.sort((a, b) => {
+//     if (a[0][1] > b[0][1]) {
+//         return 1;
+//     }
+//     if (a[0][1] < b[0][1]) {
+//         return -1;
+//     }
+//     return 0;
+// });
 
-console.table(bitCats);
+// console.table(bitCats);
 
-console.log('******************* 5 ******************');
+// console.log('******************* 5 ******************');
 
-const happyCats = [];
+// const happyCats = [];
 
-const bitCatsCopy = bitCats.slice(); // Kopijavimas
+// const bitCatsCopy = bitCats.slice(); // Kopijavimas
 
-bitGirls.forEach(girl => {
-    const catName = (girl == 'Nausėda') ? 'Bebras' : bitCatsCopy.shift()[0];
-    happyCats.push([girl, catName]);
-});
+// bitGirls.forEach(girl => {
+//     const catName = (girl == 'Nausėda') ? 'Bebras' : bitCatsCopy.shift()[0];
+//     happyCats.push([girl, catName]);
+// });
 
-console.table(happyCats);
+// console.table(happyCats);
 
-console.log(bitCats);
+// console.log(bitCats);
+
+
+const house = {};
+
+house.people = ['Nausėda', 'Bebras', 'Antanas'];
+house.tube = [1, 2];
+house.address = 'Guobų g. 13';
+
+
+// console.table(house);
+// console.log(house.tube);
+
+const catOwner = {};
+
+catOwner.girl = bitGirls[rand(0, bitGirls.length - 1)];
+catOwner.cat = cats[rand(0, cats.length - 1)];
+
+catOwner.pills = {};
+
+catOwner.pills.red = !rand(0, 1);
+catOwner.pills.blue = !rand(0, 1);
+
+console.log(catOwner);
