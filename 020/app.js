efe922c73c547129d0d9b70c64e947465d22b051
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-25 19:02:09 +0200

    i3

--- 020/app.js
+++ 020/app.js
@@ -10,3 +10,24 @@ const b1 = document.querySelector('.b1');
 b1.addEventListener('click', () => {
     console.log(i1.value);
 });
+
+
+const i2 = document.querySelector('.i2');
+const b2 = document.querySelector('.b2');
+const a2 = [];
+
+b2.addEventListener('click', () => {
+    a2.push(i2.value);
+    console.log(a2);
+});
+
+
+const i31 = document.querySelector('.i31');
+const i32 = document.querySelector('.i32');
+const b3 = document.querySelector('.b3');
+const a3 = [];
+
+b3.addEventListener('click', () => {
+    a3.push([i31.value, i32.value]);
+    console.log(a3);
+});
