16d4c3226f0a9b3f49686c870e848e4fe19f0ec7
A-Kija <arvydas.kijakauskas@gmail.com>
2022-02-25 18:34:57 +0200

    input

---
+++ 020/app.js
@@ -0,0 +1,12 @@
+const i1 = document.querySelector('.i1');
+const b1 = document.querySelector('.b1');
+
+
+// i1.addEventListener('input', () => {
+//     console.log(i1.value);
+// });
+
+
+b1.addEventListener('click', () => {
+    console.log(i1.value);
+});
