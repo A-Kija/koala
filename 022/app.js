ec3dd282976389b5df7a173eb5f16c7905e719b2
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-01 19:06:58 +0200

    this 3

--- 022/app.js
+++ 022/app.js
@@ -2,12 +2,9 @@ const flat = {};
 
 flat.color = 'pink';
 
-flat.hello = function() {
-    console.log('Hello');
-}
-
 flat.whatColor = function() {
     console.log(this.color);
 }
+
 
 flat.whatColor();
