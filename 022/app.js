785e3ebd5ddeda0f30e9ebe0aa9eece5b535bbbd
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-01 19:23:22 +0200

    class

--- 022/app.js
+++ 022/app.js
@@ -1,10 +1,34 @@
-const flat = {};
+// const flat = {}; // objekto kūrimas
 
-flat.color = 'pink';
+// flat.color = 'pink'; // savybės pridėjimas
+// flat.count = 45;
 
-flat.whatColor = function() {
-    console.log(this.color);
+// flat.whatColor = function() { // savybės spausdinimo metodo prodėjimas
+//     console.log(this.color);
+// }
+
+// flat.count10 = function() {
+//     console.log(this.count * 10);
+// }
+
+
+
+
+class myFlat {
+    constructor() {
+        this.color = 'pink';
+        this.count = 45;
+    }
+    whatColor() {
+        console.log(this.color);
+    };
+    count10() {
+        console.log(this.count * 10);
+    };
 }
 
+const flat = new myFlat(); // objekto kūrimas
 
-flat.whatColor();
+
+flat.whatColor(); // metodo naudojimas
+flat.count10();
