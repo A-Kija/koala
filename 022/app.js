465038068249208abce6f7cf959743fd0ae3cebe
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-01 19:05:37 +0200

    this 2

--- 022/app.js
+++ 022/app.js
@@ -1,5 +1,3 @@
-console.log(this);
-
 const flat = {};
 
 flat.color = 'pink';
@@ -8,31 +6,8 @@ flat.hello = function() {
     console.log('Hello');
 }
 
-
 flat.whatColor = function() {
     console.log(this.color);
-    console.log(flat.color);
 }
 
-
-// flat.labas1 = function() {
-//     console.log('labas 1', this);
-// }
-
-
-// flat.labas2 = () => {
-//     console.log('labas 2', this);
-// }
-
-
-// console.log(flat);
 flat.whatColor();
-
-
-// flat.labas1();
-// flat.labas2();
-
-
-// function flatColor() {
-//     console.log(flat.color);
-// }
