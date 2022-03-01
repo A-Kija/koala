a5625435d0583b46571b7230e6bb4f340625eb73
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-01 20:12:51 +0200

    kibiras1

--- 022/app.js
+++ 022/app.js
@@ -14,9 +14,10 @@
 class myFlat {
 
     // savybės
-    constructor(c) {
+    constructor(c, d, avinai) {
         this.color = c;
-        this.count = 45;
+        this.count = d;
+        this.avinuSkaicius = avinai;
     }
 
     // metodai
@@ -30,10 +31,45 @@ class myFlat {
 
 }
 
-const flat = new myFlat('pink'); // objekto kūrimas
+const flat = new myFlat('pink', 47, 88); // objekto kūrimas
 flat.whatColor(); // metodo naudojimas
 flat.count10();
 
-const flat23 = new myFlat('green'); // objekto kūrimas
+const flat23 = new myFlat('green', 81, 109); // objekto kūrimas
 flat23.whatColor(); // metodo naudojimas
 flat23.count10();
+
+console.log(flat, flat23);
+
+
+class Kibiras1 {
+
+    constructor() {
+        this.akmenuKiekis = 0;
+    }
+
+    prideti1Akmeni() {
+        this.akmenuKiekis++;
+    }
+
+    pridetiDaugAkmenu(kiekis) {
+        this.akmenuKiekis = this.akmenuKiekis + kiekis;
+    }
+
+    kiekPririnktaAkmenu() {
+        console.log('Pririnkta ' + this.akmenuKiekis + ' akmenų');
+    }
+
+}
+
+const kibir1 = new Kibiras1();
+const kibir2 = new Kibiras1();
+
+kibir1.prideti1Akmeni();
+kibir2.pridetiDaugAkmenu(15);
+kibir2.prideti1Akmeni();
+kibir2.prideti1Akmeni();
+kibir2.prideti1Akmeni();
+
+kibir1.kiekPririnktaAkmenu();
+kibir2.kiekPririnktaAkmenu();
