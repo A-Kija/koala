c887088d7747a03828147f1e7eb07fc67b5a3213
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-02 19:25:51 +0200

    id

--- 023/app.js
+++ 023/app.js
@@ -1,16 +1,27 @@
-const getId = () => {
-    let id = localStorage.getItem('vetId');
-    if (id === null) {
-        localStorage.setItem('vetId', 1);
-        return 1;
+class Animal {
+
+    constructor(n, t, w, id) {
+        this.name = n;
+        this.type = t;
+        this.weight = w;
+        this.id = typeof id == 'undefined' ? this.getId() : id;
     }
-    id = parseInt(id);
-    id++;
-    localStorage.setItem('vetId', id);
-    return id;
+
+    getId() {
+        let id = localStorage.getItem('vetId');
+        if (id === null) {
+            localStorage.setItem('vetId', 1);
+            return 1;
+        }
+        id = parseInt(id);
+        id++;
+        localStorage.setItem('vetId', id);
+        return id;
+    }
+
+
 }
 
 
-console.log(getId());
-console.log(getId());
-console.log(getId());
+console.log(new Animal('MurN', 'c', 5));
+console.log(new Animal('MurS', 'c', 5, 78));
