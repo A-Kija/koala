7761ad03760a0f688f0026411cf34feed3d94257
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-04 19:08:38 +0200

    zuikiai

--- 025/app.js
+++ 025/app.js
@@ -35,3 +35,49 @@ nr6.islipa(10);
 nr6.vaziuoja();
 nr6.islipa(10);
 nr6.vaziuoja();
+
+
+class Zuikis {
+
+    static visoMorku = 0;
+    static visoKopustu = 0;
+
+    static skaiciuotiVisusKopustus(kiek) {
+        this.visoKopustu += kiek; // this -> i klase
+        Zuikis.visoKopustu += kiek;
+    }
+
+    constructor() {
+        this.morkos = 0;
+        this.kopustai = 0;
+    }
+
+    duotiMorku(kiek) {
+        Zuikis.visoMorku = Zuikis.visoMorku + kiek;
+        this.morkos = this.morkos + kiek;
+    }
+
+    duotiKopustu(kiek) {
+        Zuikis.skaiciuotiVisusKopustus(kiek);
+        this.kopustai = this.kopustai + kiek; // this -> i objekta
+    }
+
+}
+
+
+
+const z1 = new Zuikis();
+const z2 = new Zuikis();
+
+z2.duotiMorku(5);
+
+z1.duotiKopustu(8);
+
+console.log(z1.morkos, z2.morkos);
+
+const z3 = new Zuikis();
+
+z3.duotiMorku(10);
+
+
+console.log(Zuikis.visoMorku, Zuikis.visoKopustu);
