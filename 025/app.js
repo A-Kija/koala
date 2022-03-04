8e01f58ee7885d7c758782d48d12ff5a4dca08cf
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-04 18:37:54 +0200

    Troleibusas

--- 025/app.js
+++ 025/app.js
@@ -4,4 +4,34 @@ const b = document.querySelector('button');
 
 b.addEventListener('click', () => {
     console.log(i.value);
-})
+});
+
+class Troleibusas {
+
+    constructor() {
+        this.keleiviuSkaicius = 0;
+    }
+
+    ilipa(keleiviuSkaicius) {
+        this.keleiviuSkaicius += keleiviuSkaicius;
+    }
+
+    islipa(keleiviuSkaicius) {
+        this.keleiviuSkaicius = Math.max(0, this.keleiviuSkaicius - keleiviuSkaicius);
+    }
+
+    vaziuoja() {
+        console.log(`Šiuo metu troleibus važiuoja
+         ${this.keleiviuSkaicius} keleiviai`);
+    }
+}
+
+const nr6 = new Troleibusas();
+
+nr6.ilipa(6);
+nr6.vaziuoja();
+nr6.ilipa(6);
+nr6.islipa(10);
+nr6.vaziuoja();
+nr6.islipa(10);
+nr6.vaziuoja();
