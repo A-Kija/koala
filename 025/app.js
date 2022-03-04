9b5ce384e694c9909d6bbd341d12707170bf016d
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-04 19:54:20 +0200

    statinis troleibusas

--- 025/app.js
+++ 025/app.js
@@ -8,33 +8,49 @@ b.addEventListener('click', () => {
 
 class Troleibusas {
 
+    static visiKeleiviai = 0;
+
+    static bendrasKeleiviuSkaicius(keleiviuSkaicius) {
+        this.visiKeleiviai += keleiviuSkaicius;
+    }
+
     constructor() {
         this.keleiviuSkaicius = 0;
     }
 
     ilipa(keleiviuSkaicius) {
         this.keleiviuSkaicius += keleiviuSkaicius;
+        Troleibusas.bendrasKeleiviuSkaicius(keleiviuSkaicius);
     }
 
     islipa(keleiviuSkaicius) {
         this.keleiviuSkaicius = Math.max(0, this.keleiviuSkaicius - keleiviuSkaicius);
+        Troleibusas.bendrasKeleiviuSkaicius(-keleiviuSkaicius);
     }
 
     vaziuoja() {
         console.log(`Šiuo metu troleibus važiuoja
          ${this.keleiviuSkaicius} keleiviai`);
     }
+
+    keleiviuSkaiciusVisuoseTroleibusuose() {
+        console.log(`Šiuo metu visais troleibusais važiuoja
+        ${Troleibusas.visiKeleiviai} keleiviai`);
+    }
 }
 
 const nr6 = new Troleibusas();
+const nr8 = new Troleibusas();
 
-nr6.ilipa(6);
-nr6.vaziuoja();
-nr6.ilipa(6);
-nr6.islipa(10);
-nr6.vaziuoja();
-nr6.islipa(10);
-nr6.vaziuoja();
+nr6.ilipa(5);
+nr6.ilipa(5);
+nr8.ilipa(10);
+nr8.islipa(5);
+nr6.keleiviuSkaiciusVisuoseTroleibusuose();
+// nr6.islipa(10);
+// nr6.vaziuoja();
+
+
 
 
 class Zuikis {
