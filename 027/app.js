001b7ade9f419db2933f16b5995f05267e0f7c23
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-08 20:13:36 +0200

    sort

--- 027/app.js
+++ 027/app.js
@@ -8,7 +8,29 @@ const render = m => {
     return html;
 }
 
+const sortArray = (m, d) => {
+    m.sort(function(a, b) {
+        if (a < b) {
+            return -1 * d;
+        }
+        if (a > b) {
+            return 1 * d;
+        }
+        return 0;
+    });
+}
+
 const ul = document.querySelector('ul');
+const sortButton = document.querySelector('#sort');
+
+sortButton.addEventListener('click', () => {
+    let direction = parseInt(sortButton.dataset.sortAs);
+    console.log(direction);
+    sortArray(products, direction);
+    direction = direction * -1;
+    sortButton.dataset.sortAs = direction;
+    ul.innerHTML = render(products);
+})
 
 
 ul.innerHTML = render(products);
