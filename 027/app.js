17373ebb901eaab52aa3b879ac44811d72dff5ec
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-08 19:30:13 +0200

    list

---
+++ 027/app.js
@@ -0,0 +1,14 @@
+const products = ['Dviratis', 'Triratis', 'Žvakidė', 'Stalas', 'Kėdė', 'Knygų lentyna', 'Paveikslas su rėmu', 'Veidrodis', 'Keturių vietų palapinė', 'Pripučiama valtis', 'Meškerė karosams gaudyti', 'Planšetė', 'Pastatoma lempa', 'Vazonas', 'Baterijų pakrovėjas', 'Pagalvių komplektas', 'Oro drėkintuvas', 'Oro sausintuvas', 'Kavinukas elektrinis', 'Kilimas 2 X 3 metrų', 'Kilimėlis kojoms valyti', 'Užuolaidos', 'Vėjo malūnas', 'Dažai sienoms', 'Dažai luboms', 'Medinė dėžė'];
+
+const render = m => {
+    let html = '';
+    m.forEach(p => {
+        html += `<li>${p}</li>`;
+    });
+    return html;
+}
+
+const ul = document.querySelector('ul');
+
+
+ul.innerHTML = render(products);
