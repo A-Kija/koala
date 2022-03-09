453c79c311d296499b6a6729fab31b3e8b037f2c
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-09 19:50:52 +0200

    get post

--- 028/app.js
+++ 028/app.js
@@ -1,10 +1,23 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
+const postId = document.querySelector('#post-id');
+const getPost = document.querySelector('#get-post');
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
+fetch('https://jsonplaceholder.typicode.com/posts/7')
+    .then(response => response.json()) // JSON Parse
     .then(data => {
         console.log(data);
         h1.innerText = data.title;
         p.innerText = data.body;
     });
+
+
+getPost.addEventListener('click', () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/' + postId.value)
+        .then(response => response.json()) // JSON Parse
+        .then(data => {
+            console.log(data);
+            h1.innerText = data.title;
+            p.innerText = data.body;
+        });
+})
