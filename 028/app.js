be413f731632b4d493bc9b7302749826116e4b13
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-09 21:14:09 +0200

    Vinted

--- 028/app.js
+++ 028/app.js
@@ -45,13 +45,41 @@ fetch('https://picsum.photos/v2/list')
 
 
 
-fetch('https://picsum.photos/v2/list')
+// fetch('https://picsum.photos/v2/list')
+//     .then(response => response.json())
+//     .then(data => {
+//         const div = document.createElement('div');
+//         data.forEach(element => {
+//             const img = document.createElement('img');
+//             img.setAttribute('src', element.download_url);
+//             div.appendChild(img);
+//         });
+//         document.querySelector('body').appendChild(div);
+//     });
+
+
+fetch('https://in3.dev/vinted/api/products/19')
     .then(response => response.json())
     .then(data => {
+
+
         const div = document.createElement('div');
-        data.forEach(element => {
+        const h1 = document.createElement('h1');
+        div.appendChild(h1);
+        h1.appendChild(document.createTextNode(data.title));
+        // PIRMAS IMG
+        const mainImgUrl = data.img[data.main_img];
+        data.img.splice(data.main_img, 1); // isimam pagrindini
+        const img1 = document.createElement('img');
+        img1.setAttribute('src', mainImgUrl);
+        img1.classList.add('big');
+        div.appendChild(img1);
+
+
+        data.img.forEach(image => {
             const img = document.createElement('img');
-            img.setAttribute('src', element.download_url);
+            img.setAttribute('src', image);
+            img.classList.add('small');
             div.appendChild(img);
         });
         document.querySelector('body').appendChild(div);
