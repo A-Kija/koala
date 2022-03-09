89737f1fb78f69b17e29930f6bbc1899f8bd27bd
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-09 19:11:54 +0200

    post 1

--- 028/app.js
+++ 028/app.js
@@ -1,7 +1,10 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+
 fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        // cia
-
+        h1.innerText = data.title;
+        p.innerText = data.body;
     });
